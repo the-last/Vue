9589e0ac262fe091ca653549e793b31162d34e9c
the-last <1039107092@qq.com>
2019-09-01 16:07:57 +0800

    删除扩展组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="transition-auto" alt="Vue logo" src="./assets/logo.png">
     
     <p>
       count from store and computing 同步计算斐波那契数  ... {{ flagFromStore }}
@@ -8,46 +8,18 @@
     <p>
       {{ doneTodos('hi---',1) }}
     </p>
-    <app2></app2>
     <router-view></router-view>
     
   </div>
 </template>
 
 <script>
-import App2 from './App2.vue';
 import './App.css';
 import { mapGetters } from 'vuex';
 import store from './store/index';
-// const app = 'App'
 
 export default {
   
-  // beforeCreate() {
-  //   console.log(app + '---beforeCreate--')
-  // },
-  // created() {
-  //   console.log(app + '---created---')
-  // },
-  // beforeMount() {
-  //   console.log(app + '---beforeMount---')
-  // },
-  // mounted() {
-  //   console.log(app + '---mounted---')
-  // },
-  // beforeUpdate() {
-  //   console.log(app + '---beforeUpdate---')
-  // },
-  // updated() {
-  //   console.log(app + '---updated---')
-  // },
-  // beforeDestroy() {
-  //   console.log(app + '---beforeDestory---')
-  // },
-  // destroyed() {
-  //   console.log(app + '---destroyed---')
-  // },
-  mixins: [App2],
   car: 'LUHU',
   created () {
     console.log('%c car App  : -- ','color:#f00;font-size:16px;', this.$options.car);
@@ -70,10 +42,25 @@ export default {
       return this.fib(num - 1) + this.fib(num - 2);
     }
   },
+
+  mounted () {
+    this.$nextTick(() => {
+       let target = document.getElementById('transition-switch');
+       target.addEventListener('transitionend', (e) => {
+         e.stopPropagation();
+         console.log('finish---');
+       }, false);
+
+       let pra = document.getElementById('app');
+       pra.addEventListener('transitionend', () => {
+         console.log('父级  finish---');
+       }, false);
+    })
+  }
 }
 </script>
 
-<style>
+<style lang="less" >
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -81,5 +68,39 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .transition-forever {
+    display: block;
+    transition: .5s 999999s;
+    width: 100px;
+    height: auto;
+    &:hover {
+      width: 50px;
+      height: auto;
+      transition: .5s width ease-in-out .5s;
+    }
+  }
+  #transition-switch {
+    display: block;
+    transition: all 1s ease-in-out;
+    padding: 1px;
+    width: 100px;
+    height: auto;
+    &:hover {
+      padding: 5px;
+      transform: scale(.5);
+    }
+  }
+  .transition-auto {
+    display: block;
+    transition: width 1s ease-in-out;
+    padding: 1px;
+    width: 80px;
+    height: auto;
+    &:hover {
+      width: auto;
+    }
+  }
 }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,53 @@
 import Vue from 'vue'
 import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import { sync } from 'vuex-router-sync';
 import App from './App.vue'
 import store from './store';
+import router  from './router';
 
+require('./mock/mock');
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://mockjs.com/api'
+Vue.prototype.$http = axios;
+Vue.config.silent = true; // 取消vue组件内所有日志和警告,不包括vuex
+
+
+// 自定义选项中，合并策略的问题,  在组件继承的范围内。
+Vue.config.optionMergeStrategies.car = function (parentValue,  childValue) {
+	
+	console.log('mixins 可能是写在子组件 - car', 'parentValue -- ', parentValue, 'childValue -- ', childValue);
+	console.log('\n\n');
+	console.log('------------------------------')
+    return  childValue ?  childValue : parentValue
+}
+
+// 阻止在启动时，生成生产提示
 Vue.config.productionTip = false
+
+// 获取组件中具体的报错信息
+Vue.config.errorHandler = function (err, vm, info) {
+	// handle error
+	// `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+	// 只在 2.2.0+ 可用
+	console.log(err, 'err\n' );
+	console.log(vm, 'vm\n' );
+	console.log(info, 'info\n' );
+}
+
+
+Vue.use(VueRouter);
 Vue.use(Vuex);
 
+// 同步路由和Store
+sync(store, router);
+
 new Vue({
-  render: h => h(App),
-  store,
-}).$mount('#app')
+	store,
+	router,
+	render: h => h(App),
+	// template: '<App />' 运行时模式不支持魔板
+	// components: { App }
+}).$mount('#app');
+
